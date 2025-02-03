e7ffbfa0d24b1f2321ff24bcca92f23366dcf4da
ttdyce <ttdyce@gmail.com>
2025-02-04 01:18:52 +0800

    try actions/exec

--- dist/index.js
+++ dist/index.js
@@ -31829,21 +31829,26 @@ module.exports = parseParams
 var __webpack_exports__ = {};
 const core = __nccwpck_require__(2831);
 const github = __nccwpck_require__(5371);
+const exec = __nccwpck_require__(5615);
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+main();
+
+async function main(){
+  try {
+    await exec.exec('npm install')
+  
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
-
-
 module.exports = __webpack_exports__;
 /******/ })()
 ;
